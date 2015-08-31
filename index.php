b28939a0889b7b70f628f1fd4d2c546f1b187db2
Alan Mabry <alan.mabry@gmail.com>
2015-08-30 21:35:00 -0700

    Update

--- index.php
+++ index.php
@@ -23,6 +23,7 @@
     </script>
 </head>
 <body>
+    <?php include_once("../ga.php") ?>
     <div id="infoBar">
         <a id="soundCloudConnect" class="leftey" href="javascript:void(0);" title="Connect with SoundCloud"></a>
         <div class="rightey">
@@ -54,12 +55,4 @@
         </div>
     </div>
 </body>
-<script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-43046605-1', 'alanmabry.com');
-    ga('send', 'pageview');
-</script>
 </html>
